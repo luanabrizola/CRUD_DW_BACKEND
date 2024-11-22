78c5207ca61360d8baea391c24ee931b91e2b650
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2024-11-22 09:59:15 -0300

    Aula2211
    Código back-end do professor para o trabalho final

---
+++ db.js
@@ -0,0 +1,7 @@
+import postgres from 'postgres';
+import 'dotenv/config'
+
+ export const sql = postgres(
+    process.env.URL,
+    {ssl: 'require'}
+)
---
+++ server.js
@@ -0,0 +1,50 @@
+import fastify from "fastify";
+import 'dotenv/config'
+import { DatabasePostgres } from "./database-postgres";
+
+//istancia o servidor 
+const server = fastify()
+//instancia o banco de dados
+const database = new DatabasePostgres();
+
+server.post("/videos", async (request, reply) => {
+    const { title, description, duration } = request.body;
+  
+    await database.create({
+      title: title,
+      description: description,
+      duration: duration,
+    });
+  
+    return reply.status(201).send();
+  });
+  
+  server.get("/videos", async (request, reply) => {                                    
+    const videos = await database.list(search);
+    return videos;
+  });
+  
+  server.put("/videos/:id", async (request, reply) => {
+    const videoId = request.params.id;
+    const { title, description, duration } = request.body;
+  
+    await database.update(videoId, {
+      title,
+      description,
+      duration,
+    });
+  
+    return reply.status(204).send();
+  });
+  
+  server.delete("/videos/:id", async (request, reply) => {
+    const videoId = request.params.id;
+  
+    await database.delete(videoId);
+    return reply.status(204).send();
+  });
+  
+  server.listen({
+    host: "0.0.0.0", // configuração para funcionar no render
+    port: process.env.PORT ?? 3333, // configuração para funcionar no render
+  });
